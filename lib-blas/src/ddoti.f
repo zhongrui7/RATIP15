6f341bf8ada9f03c10e9600155302ffdd695f311
zhongrui7 <38693476+zhongrui7@users.noreply.github.com>
2026-07-07 12:06:10 -0400

    Add files via upload

---
+++ lib-blas/src/ddoti.f
@@ -0,0 +1,73 @@
+      double precision function   ddoti   ( nz, x, indx, y )
+C
+C     ==================================================================
+C     ==================================================================
+C     ====  DDOTI -- DOUBLE PRECISION INDEXED DOT PRODUCT           ====
+C     ==================================================================
+C     ==================================================================
+C
+C     PURPOSE
+C     -------
+C
+C         DDOTI COMPUTES THE VECTOR INNER PRODUCT OF 
+C             A DOUBLE PRECISION SPARSE VECTOR  X
+C             STORED IN COMPRESSED FORM  (X,INDX) 
+C         WITH 
+C             A DOUBLE PRECISION VECTOR  Y  IN FULL STORAGE FORM.
+C
+C         ONLY THE ELEMENTS OF Y WHOSE INDICES ARE LISTED IN INDX 
+C         ARE REFERENCED.
+C
+C     ARGUMENTS
+C     ---------
+C
+C     INPUT ...
+C
+C         NZ      INTEGER     NUMBER OF ELEMENTS IN THE COMPRESSED FORM.
+C         X       DOUBLE      ARRAY CONTAINING THE VALUES OF THE 
+C                             COMPRESSED FORM.
+C         INDX    INTEGER     ARRAY CONTAINING THE INDICES OF THE
+C                             COMPRESSED FORM.  
+C         Y       DOUBLE      ARRAY, ON INPUT, WHICH CONTAINS THE 
+C                             VECTOR  Y  IN FULL STORAGE FORM.  ONLY
+C                             THE ELEMENTS  CORRESPONDING TO THE
+C                             INDICES IN  INDX  WILL BE ACCESSED.
+C
+C     OUTPUT ...
+C
+C         DDOTI   DOUBLE      DOUBLE PRECISION FUNCTION VALUE EQUAL TO
+C                             THE VECTOR INNER PRODUCT.  
+C                             IF  NZ .LE. 0  DDOTI IS SET TO ZERO.
+C
+C     SPARSE BASIC LINEAR ALGEBRA SUBPROGRAM
+C
+C     FORTRAN VERSION WRITTEN OCTOBER 1984
+C     ROGER G GRIMES, BOEING COMPUTER SERVICES
+C
+C     ==================================================================
+C
+C     -------------
+C     ... ARGUMENTS
+C     -------------
+C
+      INTEGER             NZ, INDX (*)
+C
+      DOUBLE PRECISION    X (*), Y (*)
+C
+C     -------------------
+C     ... LOCAL VARIABLES
+C     -------------------
+C
+      INTEGER             I
+C
+C     ==================================================================
+C
+      DDOTI = 0.0D0
+      IF  ( NZ .LE. 0 )  RETURN
+C
+      DO 10 I = 1, NZ
+          DDOTI = DDOTI  +  X(I) * Y(INDX(I))
+   10 CONTINUE
+C
+      RETURN
+      END
